13c5401d055045ca99b13c654caa328569d9e3f9
Zach Young <zachyoung@Zachs-iMac.local>
2021-01-25 16:13:21 -0600

    saving endpoints

---
+++ related-products/index.js
@@ -0,0 +1,4 @@
+const product_sku_csv = ['pi_test_1', 'pi_test_2', 'pi_test_3', 'pi_test_4' ,'pi_test_5']
+
+//https://api.bigcommerce.com/stores/bnr41x692p/v3/catalog/products?sku=pi_test_1
+//https://api.bigcommerce.com/stores/bnr41x692p/v3/catalog/products?id=1076
