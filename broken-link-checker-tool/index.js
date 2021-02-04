741068901c550189d26efa7d0578d6f3aebb38c9
Zach Young <zachyoung@Zachs-iMac.local>
2021-02-04 11:09:09 -0600

    working on node/react broken links

--- broken-link-checker-tool/index.js
+++ broken-link-checker-tool/index.js
@@ -1,28 +1,4 @@
-var { SiteChecker } = require("broken-link-checker");
-
-const siteChecker = new SiteChecker(
-    { 
-        excludeInternalLinks: false,
-        excludeExternalLinks: false, 
-        filterLevel: 0,
-        acceptedSchemes: ["http", "https"],
-        excludedKeywords: ["linkedin"]
-    },
-    {
-        "error": (error) => {
-            console.error(error);
-        },
-        "link": (result, customData) => {
-            if(result.broken) {
-                if(result.http.response && ![undefined, 200].includes(result.http.response.statusCode)) {
-                    console.log(`${result.http.response.statusCode} => ${result.url.original}`);
-                }
-            }
-        },
-        "end": () => {
-            console.log("COMPLETED!");
-        }
-    }
-);
-
-siteChecker.enqueue("https://www.servocity.com/");
+require('dotenv').config()
+const server = require('./server.js');
+const port = process.env.PORT || 5000;
+server.listen(port, () => console.log(`\n** Running on port ${port} **\n`));
--- react-link/broken-links/src/App.js
+++ react-link/broken-links/src/App.js
@@ -1,12 +1,46 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import { Input, Select } from 'antd';
+const { Option } = Select;
+const selectBefore = (
+  <Select defaultValue="http://" className="select-before">
+    <Option value="http://">http://</Option>
+    <Option value="https://">https://</Option>
+  </Select>
+);
+const selectAfter = (
+  <Select defaultValue=".com" className="select-after">
+    <Option value=".com">.com</Option>
+    <Option value=".jp">.jp</Option>
+    <Option value=".cn">.cn</Option>
+    <Option value=".org">.org</Option>
+  </Select>
+);
+
+
+const useEventSource = (url) => {
+  const [data, updateData] = useState([]);
+
+  useEffect(() => {
+      const source = new EventSource(url);
+
+      source.onmessage = function logEvents(event) {     
+        console.log(event.data) 
+          updateData(prev => [...prev, JSON.parse(event.data)]);     
+      }
+  }, [])
+
+  return data;
+}
 
 function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+const data = useEventSource('http://localhost:5000/');
+if (!data) {
+  return <div />;
 }
+
+return <div className="container">{data&&data.map(el => <p>{el}</p>)}</div>;
+}
+
 
 export default App;
