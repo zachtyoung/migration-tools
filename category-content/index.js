124e988bf80f5e1151d46ac4319acf7429df4395
Zach Young <zachyoung@Zachs-iMac.local>
2021-03-02 11:07:00 -0600

    category descriptions updating correctly.

--- category-content/index.js
+++ category-content/index.js
@@ -1,19 +1,25 @@
 require('dotenv').config()
+const csv=require('csvtojson');
+const csvFilePath='./categories.csv'
 const axios = require("axios");
 
+
 let sourceStoreHash = 'bnr41x692p'
-let sourceStoreName = 'servocity_staging'
-let destinationStore='yy9d3il1gg'
+let sourceStore = 'GOBILDA_STAGING'
+let destinationStoreHash='yy9d3il1gg'
+let destinationStore='SERVOCITY_STAGING'
 
 
-const getCategoryContent = async () =>{
+
+const getCategoryContent = async (storeHash, storeName) =>{
+    storeName = storeName.toUpperCase()
     const buffer = []
     let next = ""
     do {
-      let response = await axios.get(`https://api.bigcommerce.com/stores/${sourceStoreHash}/v3/catalog/categories?limit=250${next}`, {
+      let response = await axios.get(`https://api.bigcommerce.com/stores/${storeHash}/v3/catalog/categories?limit=250${next}`, {
         headers:{
-          'X-Auth-Client':process.env.GOBILDA_STAGING_CLIENT,
-          'X-Auth-Token':process.env.GOBILDA_STAGING_TOKEN
+          'X-Auth-Client':process.env[storeName + '_CLIENT'],
+          'X-Auth-Token':process.env[storeName + '_TOKEN']
         }})
         buffer.push(...response.data.data)
         next = response.data.meta.pagination.links.next
@@ -21,10 +27,74 @@ const getCategoryContent = async () =>{
     return buffer
   }
 
+const getCustomTemplateAssociations = async (storeHash, storeName) =>{
+    storeName = storeName.toUpperCase()
+      let response = await axios.get(`https://api.bigcommerce.com/stores/${storeHash}/v3/storefront/custom-template-associations?limit=250`, {
+        headers:{
+          'X-Auth-Client':process.env[storeName + '_CLIENT'],
+          'X-Auth-Token':process.env[storeName + '_TOKEN']
+        }})
+        return response.data.data
+}
+const updateCustomTemplateAssociations = async()=>{
 
+}
+const updateCategories = async (storeHash, storeName, categoriesArray) =>{
+  for(var i in categoriesArray){
+    axios.put(`https://api.bigcommerce.com/stores/${storeHash}/v3/catalog/categories/${categoriesArray[i].destination_id}`,{
+      "description":categoriesArray[i].description
+    }, {
+      headers:{
+        'X-Auth-Client':process.env[storeName + '_CLIENT'],
+        'X-Auth-Token':process.env[storeName + '_TOKEN']
+      }})
+      .then(res => console.log(res))
+  }
+}
 
-getCategoryContent()
+let categoryNameAndId = []
+let sourceStoreTemplates
+let destinationStoreTemplates
+getCategoryContent(sourceStoreHash, sourceStore)
 .then(res => {
-    console.log(res)
+    csv().fromFile(csvFilePath)
+    .then((jsonObj)=>{
+        for (var i in jsonObj) {
+            for(var j in res){
+                if(res[j].name == jsonObj[i].Name){
+                   categoryNameAndId.push({"name":res[j].name, "source_id":res[j].id, "description":res[j].description, "template":""})
+                }
+            }
+        }
+    })
+    .then(async () =>{
+      sourceStoreTemplates =  await getCustomTemplateAssociations(sourceStoreHash, sourceStore)
+      destinationStoreTemplates = await getCustomTemplateAssociations(destinationStoreHash, destinationStore)
+      for (var i in categoryNameAndId) {
+        for (var j in sourceStoreTemplates) {
+          if(categoryNameAndId[i].source_id == sourceStoreTemplates[j].entity_id){
+            categoryNameAndId[i].template = sourceStoreTemplates[j].file_name
+          }
+    
+        }
+      }
+
+    })
+    .then(() =>{
+      getCategoryContent(destinationStoreHash, destinationStore)
+      .then(res =>{
+        for (var i in categoryNameAndId) {
+          for(var j in res){
+              if(res[j].name == categoryNameAndId[i].name){
+                 categoryNameAndId[i].destination_id = res[j].id
+              }
+          }
+      }
+      })
+      .then(() =>{
+        updateCategories(destinationStoreHash, destinationStore, categoryNameAndId)
+      })
+     
+    })
     
 })
